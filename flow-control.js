f8e0d117ad2bf04b596bb778a2f5ad14ca32395f
Teejay Williams <th3dom1nofx@gmail.com>
2019-03-19 00:23:05 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,7 +7,7 @@ function basicTeenager(age){
   console.log()
 }
 
-function teenager(age) {if (age<=13||age>=19)
+function teenager(age) {if (age<=13 && age>=19)
 console.log("You are a teenager!")
 else if (age>=12)
 console.log("You are not a teenager")
